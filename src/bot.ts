c5cd0f4288e5d8fc699e9cb4d97ca8aa6b8f9101
champnc <natdanai.dev@gmail.com>
2022-11-16 23:24:36 +0700

    add prettier

--- src/bot.ts
+++ src/bot.ts
@@ -1,36 +1,38 @@
-import {Client, GatewayIntentBits} from "discord.js";
+import { Client, GatewayIntentBits } from "discord.js";
 import "dotenv/config";
 
-const client = new Client({ intents: [
-  GatewayIntentBits.Guilds,
-  GatewayIntentBits.GuildMessages,
-  GatewayIntentBits.MessageContent
-] });
+const client = new Client({
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+	],
+});
 
 client.login(process.env.DISCORD_KEY).catch((err) => {
-  console.error("login error",err);
-  process.exit(1);
+	console.error("login error", err);
+	process.exit(1);
 });
 
 const prefix = "!";
-const randomMessage = ["ว่ายังไง","มีอะไรวะ","ว่าไงจ๊ะ"];
+const randomMessage = ["ว่ายังไง", "มีอะไรวะ", "ว่าไงจ๊ะ"];
 
 client.on("ready", () => {
-  console.log("I'm ready😄...");
+	console.log("I'm ready😄...");
 });
 
 client.on("messageCreate", function (message) {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
+	if (message.author.bot) return;
+	if (!message.content.startsWith(prefix)) return;
 
-  const commandBody = message.content.slice(prefix.length);
-  const args = commandBody.split(" ");
-  const command = args.shift()?.toLowerCase();
+	const commandBody = message.content.slice(prefix.length);
+	const args = commandBody.split(" ");
+	const command = args.shift()?.toLowerCase();
 
-  if (command === "ping") {
-    message.reply("ว่ายังไง");
-  } else if (command === "ไอฟร้อง") {
-    const random = Math.floor(Math.random() * randomMessage.length);
-    message.reply(randomMessage[random]);
-  }
+	if (command === "ping") {
+		message.reply("ว่ายังไง");
+	} else if (command === "ไอฟร้อง") {
+		const random = Math.floor(Math.random() * randomMessage.length);
+		message.reply(randomMessage[random]);
+	}
 });
